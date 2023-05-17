355da7ca55be9f2f9cd97613d3a62d210148fab6
Santiago Nahuel De los Santos <santiagodelossantos630@gmail.com>
2023-05-17 20:05:14 -0300

    Server Express

--- src/components/ProductsManager.js
+++ src/components/ProductsManager.js
@@ -1,6 +1,6 @@
 import {promises as fs} from "fs"
 
-class ProductManager {
+export default class ProductManager {
 
     constructor(){
         this.patch = "./productos.txt"
@@ -67,10 +67,18 @@ class ProductManager {
 
 }
 
-const productos = new ProductManager
+// const productos = new ProductManager
 
-// productos.addProduct("Titulo1", "Description1", 1000, "Image1", "asda11", 5)
-// productos.addProduct("Titulo2", "Description2", 1000, "Image2", "asda12", 4) 
+/*
+productos.addProduct("Titulo1", "Description1", 1000, "Image1", "asda11", 1)
+productos.addProduct("Titulo2", "Description2", 2000, "Image2", "asda12", 2) 
+productos.addProduct("Titulo3", "Description3", 3000, "Image3", "asda13", 3)
+productos.addProduct("Titulo4", "Description4", 4000, "Image4", "asda14", 4) 
+productos.addProduct("Titulo5", "Description5", 5000, "Image5", "asda15", 5)
+productos.addProduct("Titulo6", "Description6", 6000, "Image6", "asda16", 6) 
+productos.addProduct("Titulo7", "Description7", 7000, "Image7", "asda17", 7)
+productos.addProduct("Titulo8", "Description8", 9000, "Image8", "asda18", 8) 
+*/
 
 // productos.getProducts()
 
@@ -78,6 +86,7 @@ const productos = new ProductManager
 
 // productos.deleteProductsById(1)
 
+/*
 productos.updateProducts({
     title: 'Titulo3',
     description: 'Description3',
@@ -87,3 +96,4 @@ productos.updateProducts({
     stock: 4,
     id: 3
 })
+*/
